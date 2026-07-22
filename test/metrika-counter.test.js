f9fc9cf0c1389f09b1e0fe41c1bbbb54b8cf3f93
Nickolay <nickolay2008.com@gmail.com>
2026-07-22 14:58:41 +0300

    Проверять счётчик HeroStar и в HTML, и в аналитике

--- test/metrika-counter.test.js
+++ test/metrika-counter.test.js
@@ -3,11 +3,17 @@ import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 
 const analyticsUrl = new URL('../public/marketing-analytics.js', import.meta.url);
+const indexUrl = new URL('../public/index.html', import.meta.url);
 
-test('HeroStar отправляет цели только в собственный счётчик Метрики', async () => {
-  const source = await readFile(analyticsUrl, 'utf8');
+test('HeroStar использует только собственный счётчик Метрики во всех точках входа', async () => {
+  const [analyticsSource, indexSource] = await Promise.all([
+    readFile(analyticsUrl, 'utf8'),
+    readFile(indexUrl, 'utf8'),
+  ]);
+  const allSources = `${indexSource}\n${analyticsSource}`;
 
-  assert.match(source, /HERO_STAR_COUNTER_ID\s*=\s*110937602/);
-  assert.doesNotMatch(source, /110783019/);
-  assert.match(source, /metrikaMeta\.content\s*=\s*String\(HERO_STAR_COUNTER_ID\)/);
+  assert.match(indexSource, /yandex-metrika-id" content="110937602"/);
+  assert.match(analyticsSource, /HERO_STAR_COUNTER_ID\s*=\s*110937602/);
+  assert.doesNotMatch(allSources, /110783019/);
+  assert.match(analyticsSource, /metrikaMeta\.content\s*=\s*String\(HERO_STAR_COUNTER_ID\)/);
 });
