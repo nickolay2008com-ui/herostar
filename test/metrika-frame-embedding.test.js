0dc2a4f36d2cd7f0dcdb0dd5ac6e23afb7074a29
Nickolay <nickolay2008.com@gmail.com>
2026-07-22 15:11:40 +0300

    Защитить CSP-хеш официального кода Метрики тестом

--- test/metrika-frame-embedding.test.js
+++ test/metrika-frame-embedding.test.js
@@ -4,7 +4,7 @@ import { readFile } from 'node:fs/promises';
 
 const authUrl = new URL('../src/auth.js', import.meta.url);
 
-test('HTML-страницы разрешают визуальный редактор Яндекс Метрики без открытия для чужих сайтов', async () => {
+test('HTML-страницы разрешают официальный счётчик и визуальный редактор Метрики без открытия для чужих сайтов', async () => {
   const source = await readFile(authUrl, 'utf8');
 
   assert.match(source, /frame-ancestors/);
@@ -12,6 +12,7 @@ test('HTML-страницы разрешают визуальный редакт
   assert.match(source, /https:\/\/\*\.webvisor\.com/);
   assert.match(source, /https:\/\/mc\.webvisor\.com/);
   assert.match(source, /https:\/\/yastatic\.net/);
+  assert.match(source, /sha256-jp2EkOkNiGIs4JfVpE2oclfqqUq75ROwSo88kh7TP5k=/);
   assert.match(source, /mergeCspDirective\(nextPolicy, 'script-src'/);
   assert.match(source, /mergeCspDirective\(nextPolicy, 'connect-src'/);
   assert.match(source, /mergeCspDirective\(nextPolicy, 'frame-src'/);
