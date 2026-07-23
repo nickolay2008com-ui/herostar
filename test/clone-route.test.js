c2e706ae0f4e847605328c4aafcf0ae3edc9c823
Nickolay <nickolay2008.com@gmail.com>
2026-07-23 14:26:45 +0300

    test: проверить страницу клона по адресу с завершающим слешем

--- test/clone-route.test.js
+++ test/clone-route.test.js
@@ -2,10 +2,15 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import { readFile } from 'node:fs/promises';
 
-const serverUrl = new URL('../server.js', import.meta.url);
+const cloneFileUrl = new URL('../public/clone.html', import.meta.url);
+const cloneIndexUrl = new URL('../public/clone/index.html', import.meta.url);
 
-test('страница клона явно доступна с завершающим слешем и без него', async () => {
-  const server = await readFile(serverUrl, 'utf8');
-  assert.match(server, /app\.get\(\[\s*['"]\/clone['"]\s*,\s*['"]\/clone\/['"]\s*\]/);
-  assert.match(server, /sendFile\(['"]clone\.html['"]/);
+test('страница /clone/ совпадает с основной страницей клона', async () => {
+  const [cloneFile, cloneIndex] = await Promise.all([
+    readFile(cloneFileUrl, 'utf8'),
+    readFile(cloneIndexUrl, 'utf8'),
+  ]);
+  assert.equal(cloneIndex, cloneFile);
+  assert.match(cloneIndex, /<title>Звёздный клон/);
+  assert.match(cloneIndex, /src="\/clone\.js"/);
 });
