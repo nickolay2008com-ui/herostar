8408d7a06919967ca5a3eb3f693d5eae0b5cf338
Nickolay <nickolay2008.com@gmail.com>
2026-09-01 09:48:02 +0400

    Bust immutable Clone Live app cache after UI polish

--- public/clone-ui-gears.js
+++ public/clone-ui-gears.js
@@ -19,7 +19,7 @@
   if (liveInterface) {
     const appStyle = document.createElement('link');
     appStyle.rel = 'stylesheet';
-    appStyle.href = '/clone/live/live-app.css?v=20260827-app6';
+    appStyle.href = '/clone/live/live-app.css?v=20260901-contour1';
     document.head.append(appStyle);
 
     const appScript = document.createElement('script');
